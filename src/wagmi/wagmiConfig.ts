95e04a9db8a468a74ea8882c6d0c02949713e154
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2024-03-18 15:27:55 +0700

    fix: connector modal

--- src/wagmi/wagmiConfig.ts
+++ src/wagmi/wagmiConfig.ts
@@ -4,9 +4,7 @@ import { walletConnect } from "wagmi/connectors";
 
 export const wagmiConfig = createConfig({
   chains: [bsc, bscTestnet, fantom],
-  connectors: [
-    walletConnect({ projectId: "505f490de3842b110b964f72bcd7535e" }),
-  ],
+  connectors: [walletConnect({ projectId: "505f490de3842b110b964f72bcd7535e", showQrModal: false })],
   transports: {
     [bsc.id]: http(),
     [bscTestnet.id]: http(),
